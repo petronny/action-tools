32382336b7ea041f33989eb0eac87f76c62f1708
Jingbei Li <i@jingbei.li>
2020-06-11 00:04:05 +0800

    fixed initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -10,17 +10,11 @@ pacman -S --noconfirm --needed base-devel devtools dbus git \
 	jq \
 	pyalpm python-lxml python-requests python-toposort python-yaml
 
-mkdir -p ~/{packages,sources,srcpackages,makepkglogs}
 sed \
 	-e "s|MAKEFLAGS=.*|MAKEFLAGS=-j$(nproc)|" \
-	-e "s|#PKGDEST=.*|PKGDEST='$HOME/packages'|" \
-	-e "s|#SRCDEST=.*|SRCDEST='$HOME/sources'|" \
-	-e "s|#SRCPKGDEST=.*|SRCPKGDEST='$HOME/srcpackages'|" \
-	-e "s|#LOGDEST=.*|LOGDEST='$HOME/makepkglogs'|" \
 	-i /etc/makepkg.conf
 dbus-uuidgen --ensure=/etc/machine-id
-useradd -m pkgbuild -g wheel -d ~
-chown -R pkgbuild:root ~
+useradd -m pkgbuild -g wheel
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 pkgbase=$(cat "${GITHUB_EVENT_PATH}" | jq -r .action)
