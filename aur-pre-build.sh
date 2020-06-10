f1e740dd49389cf4030c1e19cf2f6972b30f317c
Jingbei Li <i@jingbei.li>
2020-06-10 18:46:45 +0800

    fixed aur-pre-build.sh

--- aur-pre-build.sh
+++ aur-pre-build.sh
@@ -2,7 +2,7 @@
 set -e
 
 rm -rf ${GITHUB_WORKFLOW}
-git checkout -- ${GITHUB_WORKFLOW}/{lilac.py,lilac.yaml}
+git checkout -- ${GITHUB_WORKFLOW}/{lilac.py,lilac.yaml} || echo
 
 cd ${GITHUB_WORKFLOW}
 
