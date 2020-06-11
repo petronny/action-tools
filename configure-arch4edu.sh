0bdc4b8dd69e972f95c1cd275fe8f43cf4bcc1ed
Jingbei Li <i@jingbei.li>
2020-06-12 01:32:43 +0800

    fixed configure-arch4edu.sh

--- configure-arch4edu.sh
+++ configure-arch4edu.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
-cp keyring/* /usr/share/pacman/keyrings/
-
-pacman-key --gpgdir ~/pacman/arch4edu/gnupg --init
-pacman-key --gpgdir ~/pacman/arch4edu/gnupg --populate arch4edu
+cp ${GITHUB_WORKSPACE}/keyring/* /usr/share/pacman/keyrings/
 
 mkdir -p ~/pacman/arch4edu
+cd ~/pacman/arch4edu
+
+pacman-key --gpgdir gnupg --init
+pacman-key --gpgdir gnupg --populate arch4edu
+
 sed \
 	-e '/^Include/d' \
 	-e '/^\[core\]/d' \
 	-e '/^\[extra\]/d' \
 	-e '/^\[community\]/d' \
 	-e '/^\[multilib\]/d' \
-	/etc/pacman.conf > ~/pacman/arch4edu/pacman.conf
-echo [arch4edu] >> ~/pacman/arch4edu/pacman.conf
-sed 's/#//' ~/mirrorlist/mirrorlist.arch4edu >> ~/pacman/arch4edu/pacman.conf
+	/etc/pacman.conf > pacman.conf
+echo [arch4edu] >> pacman.conf
+sed 's/#//' ${GITHUB_WORKSPACE}/mirrorlist/mirrorlist.arch4edu >> pacman.conf
 
-pacman -Sy --config ~/pacman/arch4edu/pacman.conf --dbpath ~/pacman/arch4edu/db
+pacman -Sy --config pacman.conf --dbpath db
