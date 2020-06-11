8b961ccc58747ca3ef7b4d107fc41eb997f92387
Jingbei Li <i@jingbei.li>
2020-06-11 22:51:32 +0800

    fixed post-build.sh

--- post-build.sh
+++ post-build.sh
@@ -5,7 +5,12 @@ cd ~/arch4edu
 
 for i in $(find . -type f -name '*.pkg.tar.zst')
 do
-	mv $i $(echo $i | sed 's/:/COLON/g')
+	filename=$(basename $i)
+	dirname=$(dirname $i)
+	if [ $(echo $filename | grep : -c) -gt 0 ]
+	then
+		mv $dirname/filename $dirname/$(echo $filename | sed 's/:/COLON/g')
+	fi
 done
 
 clean-up.sh
