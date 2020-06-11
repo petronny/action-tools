424b078a4361b3fce9684e4dfac1bd70e6d82e3b
Jingbei Li <i@jingbei.li>
2020-06-12 01:12:28 +0800

    fixed configure-arch4edu.sh

--- configure-arch4edu.sh
+++ configure-arch4edu.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 cp keyring/* /usr/share/pacman/keyrings/
-pacman-key --populate arch4edu
 
-mkdir -p ~/pacman
-cp /etc/pacman.conf ~/pacman/pacman.conf.arch4edu
-echo [arch4edu] >> ~/pacman/pacman.conf.arch4edu
-sed 's/#//' -i ~/mirrorlist/mirrorlist.arch4edu >> ~/pacman/pacman.conf.arch4edu
+pacman-key --gpgdir ~/pacman/arch4edu/gnupg --init
+pacman-key --gpgdir ~/pacman/arch4edu/gnupg --populate arch4edu
+
+mkdir -p ~/pacman/arch4edu
+sed \
+	-e '/^Include/d' \
+	-e '/^\[core\]/d' \
+	-e '/^\[extra\]/d' \
+	-e '/^\[community\]/d' \
+	-e '/^\[multilib\]/d' \
+	/etc/pacman.conf > ~/pacman/arch4edu/pacman.conf
+echo [arch4edu] >> ~/pacman/arch4edu/pacman.conf
+sed 's/#//' ~/mirrorlist/mirrorlist.arch4edu >> ~/pacman/arch4edu/pacman.conf
+
+pacman -Sy --config ~/pacman/arch4edu/pacman.conf --dbpath ~/pacman/arch4edu/db
