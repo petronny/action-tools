c89cf674c68c26f9794481a0f8f639b6a3191595
Jingbei Li <i@jingbei.li>
2020-06-12 15:50:48 +0800

    fixed download-package-from-artifact.sh

--- download-package-from-artifact.sh
+++ download-package-from-artifact.sh
@@ -3,7 +3,6 @@ repo=$1
 pkgbase=$2
 pkgname=$3
 path=$4
-token=$5
 
 artifact_id=$(curl "https://api.github.com/repos/${repo}/actions/artifacts" |
 	jq ".artifacts | .[] | select(.name==\"${pkgbase}\") | .id" |
@@ -20,7 +19,7 @@ if [ ! -f $pkgbase.zip ]
 then
 	curl -L "https://api.github.com/repos/$repo/actions/artifacts/$artifact_id/zip" \
 		-H "Accept: application/vnd.github.everest-preview+json" \
-		-H "Authorization: token $token" \
+		-H "Authorization: token $TOKEN" \
 		-o $pkgbase.zip
 fi
 
