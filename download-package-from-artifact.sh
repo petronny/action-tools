de9bd6bef679b9b094a7f9c2b17a0f0e4210f3a7
Jingbei Li <i@jingbei.li>
2020-06-12 15:33:24 +0800

    added download-package-from-artifact.sh

---
+++ download-package-from-artifact.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+repo=$1
+pkgbase=$2
+pkgname=$3
+path=$4
+token=$5
+
+artifact_id=$(curl "https://api.github.com/repos/${repo}/actions/artifacts" |
+	jq ".artifacts | .[] | select(.name==\"${pkgbase}\") | .id" |
+	head -n1)
+
+mkdir -p $path
+cd $path
+
+bsdtar tf $pkgbase.zip || rm -f $pkgbase.zip
+
+if [ ! -f $pkgbase.zip ]
+then
+	curl -L "https://api.github.com/repos/$repo/actions/artifacts/$artifact_id/zip" \
+		-H "Accept: application/vnd.github.everest-preview+json" \
+		-H "Authorization: token $token" \
+		-o $pkgbase.zip
+fi
+
+package=$(bsdtar tf ${pkgbase}.zip | grep -P "^${pkgname}-[0-9.-]*-(x86_64|armv6h|armv7h|aarch64|any)\.pkg\.tar.*")
+bsdtar xvf ${pkgbase}.zip $package
