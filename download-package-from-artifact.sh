2f2a7a1d547907e564daffb07c67ab3d235f20cc
Jingbei Li <i@jingbei.li>
2020-06-12 16:13:15 +0800

    fixed download-package-from-artifact.sh

--- download-package-from-artifact.sh
+++ download-package-from-artifact.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -ex
+
 repo=$1
 pkgbase=$2
 pkgname=$3
@@ -17,11 +19,24 @@ bsdtar tf $pkgbase.zip || rm -f $pkgbase.zip
 
 if [ ! -f $pkgbase.zip ]
 then
+	set +x
 	curl -L "https://api.github.com/repos/$repo/actions/artifacts/$artifact_id/zip" \
 		-H "Accept: application/vnd.github.everest-preview+json" \
 		-H "Authorization: token $TOKEN" \
 		-o $pkgbase.zip
+	set -x
 fi
 
-package=$(bsdtar tf ${pkgbase}.zip | grep -P "^${pkgname}-[0-9.-]*-(x86_64|armv6h|armv7h|aarch64|any)\.pkg\.tar.*")
+package=$(bsdtar tf ${pkgbase}.zip |
+	sed 's/COLON/:/g' |
+	grep -P "^${pkgname}-[0-9.:-]*-(x86_64|armv6h|armv7h|aarch64|any)\.pkg\.tar.*" |
+	sed 's/:/COLON/g')
+
+[ -z "$package" ] && exit 1
+
 bsdtar xvf ${pkgbase}.zip $package
+
+if [ $(echo $package | grep COLON -c) -gt 0 ]
+then
+	mv $package $(echo $package | sed 's/COLON/:/g')
+fi
