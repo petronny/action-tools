d7e5e3aeb7bd2d7496786632384e4be48feb3f89
Jingbei Li <i@jingbei.li>
2020-06-12 16:24:18 +0800

    fixed configure-arch4edu.sh

--- configure-arch4edu.sh
+++ configure-arch4edu.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-cp ${GITHUB_WORKSPACE}/keyring/* /usr/share/pacman/keyrings/
 
 mkdir -p ~/pacman/arch4edu/db
 cd ~/pacman/arch4edu
+
+git clone https://github.com/arch4edu/mirrorlist
+git clone https://github.com/arch4edu/arch4edu-keyring keyring
+
+cp keyring/* /usr/share/pacman/keyrings/
 
 pacman-key --gpgdir gnupg --init
 pacman-key --gpgdir gnupg --populate arch4edu
@@ -15,6 +19,6 @@ sed \
 	-e '/^\[multilib\]/d' \
 	/etc/pacman.conf > pacman.conf
 echo [arch4edu] >> pacman.conf
-sed 's/#//' ${GITHUB_WORKSPACE}/mirrorlist/mirrorlist.arch4edu >> pacman.conf
+sed 's/#//' mirrorlist/mirrorlist.arch4edu >> pacman.conf
 
 pacman -Sy --config pacman.conf --dbpath db
