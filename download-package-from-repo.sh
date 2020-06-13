e6e0178d39da69a82f5c250f68715082455b2725
Jingbei Li <i@jingbei.li>
2020-06-13 13:18:14 +0800

    fixed download-package-from-repo.sh

--- download-package-from-repo.sh
+++ download-package-from-repo.sh
@@ -10,12 +10,5 @@ repo=~/pacman/$repo
 pacman_conf=$repo/pacman.conf
 dbpath=$repo/db
 gpgdir=$repo/gnupg
-pacman="fakeroot pacman --arch $arch --config $pacman_conf --dbpath $dbpath --gpgdir $gpgdir --cachedir $path --noconfirm"
-depends=$(LANG=C $pacman -Si $package | sed -n '/^Depends/{s/^.*://;p}')
 
-for i in $depends
-do
-	assume_installed+=" --assume-installed $i"
-done
-
-$pacman -Sw $package $assume_installed
+fakeroot pacman --arch $arch --config $pacman_conf --dbpath $dbpath --gpgdir $gpgdir --cachedir $path --noconfirm -Swdd $package
