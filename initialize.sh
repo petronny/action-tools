fa7b3eba127e5ba3e4fa1dbb6cf1509f11e223b0
Jingbei Li <i@jingbei.li>
2020-06-13 22:45:41 +0800

    fixed initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -ex
 
-find ${GITHUB_WORKSPACE}/action-tools -type f -name '*.sh' -exec cp {} /usr/bin \;
 cp -r ${GITHUB_WORKSPACE}/lilac ~
 cp ${GITHUB_WORKSPACE}/lilac/recv_gpg_keys /usr/bin
 
@@ -26,6 +25,7 @@ pkgbase=$(jq -r .action "${GITHUB_EVENT_PATH}")
 uuid=$(echo $pkgbase | cut -d' ' -f2)
 pkgbase=$(echo $pkgbase | cut -d' ' -f1)
 
+echo "::add-path::${GITHUB_WORKSPACE}/action-tools"
 echo "::set-output name=pkgbase::${pkgbase}"
 echo "::set-output name=uuid::${uuid}"
 echo "::set-output name=home::${HOME}"
