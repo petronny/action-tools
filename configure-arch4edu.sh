92def22252a0784534f87e09f7dcaff4370a88ec
Jingbei Li <i@jingbei.li>
2020-06-27 21:37:45 +0800

    better configure-arch4edu.sh

--- configure-arch4edu.sh
+++ configure-arch4edu.sh
@@ -1,17 +1,31 @@
 #!/bin/sh
-set -ex
+set -e
 
-mkdir -p ~/pacman/arch4edu/{db,gnupu}
+mkdir -p ~/pacman/arch4edu/db
 cd ~/pacman/arch4edu
 
-fakeroot pacman-key --gpgdir gnupg --init
-fakeroot pacman-key --gpgdir gnupg --recv-keys 7931B6D628C8D3BA
-fakeroot pacman-key --gpgdir gnupg --finger 7931B6D628C8D3BA
-fakeroot pacman-key --gpgdir gnupg --lsign-key 7931B6D628C8D3BA
+[ ! -d gnupg ] && fakeroot pacman-key --gpgdir gnupg --init
 
-echo [options] > pacman.conf
-pacman-conf | grep '^Architecture' >> pacman.conf
-echo [arch4edu] >> pacman.conf
-sed 's/#//' ${GITHUB_WORKSPACE}/mirrorlist/mirrorlist.arch4edu >> pacman.conf
+pacman-key --gpgdir gnupg -l 7931B6D628C8D3BA && imported=1
 
-fakeroot pacman -Sy --config pacman.conf --dbpath db --noprogressbar
+if [ -z "$imported" ]
+then
+	if [ -f /usr/share/pacman/keyrings/arch4edu-trusted ]
+	then
+		sudo pacman-key --populate arch4edu
+	else
+		sudo pacman-key --gpgdir gnupg --recv-keys 7931B6D628C8D3BA
+		sudo pacman-key --gpgdir gnupg --finger 7931B6D628C8D3BA
+		sudo pacman-key --gpgdir gnupg --lsign-key 7931B6D628C8D3BA
+	fi
+fi
+
+if [ ! -f pacman.conf ]
+then
+	echo [options] > pacman.conf
+	pacman-conf | grep '^Architecture' >> pacman.conf
+	echo [arch4edu] >> pacman.conf
+	sed 's/#//' ${GITHUB_WORKSPACE}/mirrorlist/mirrorlist.arch4edu >> pacman.conf
+fi
+
+sudo pacman -Sy --config pacman.conf --dbpath db --noprogressbar
